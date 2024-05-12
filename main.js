0d5b7fbdece86a42ab7a374790d1d529e832852e
gfortune1 <fgeee1@gmail.com>
2024-05-12 22:15:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+async function book() {
+    const place = document.getElementById('placeInput').value;
+    const date = document.getElementById('dateInput').value;
+    console.log(place, date)
+    try {
+        const response = await fetch('http://localhost:3000/bookings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ place, date }),
+        });
+
+        if (response.ok) {
+           // Show booking completed message
+        document.getElementById('bookingMessage').style.display = 'block';
+        // Optional: You can hide the form after successful booking
+        document.getElementById('bookingForm').style.display = 'none';
+        }
+
+        else {
+            const errorData = await response.json();
+            alert('Error saving booking data:\n' + JSON.stringify(errorData));
+            document.getElementById('bookingMessage').style.display = 'none';
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        alert('Booking failed. Please try again later.');
+    }
+}
